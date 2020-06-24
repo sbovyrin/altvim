d82aa0087b083eddeeb16d098343ffec91eaa614
Sergey B <s.bovyrin@icloud.com>
2020-06-25 02:53:48 +0600

    Support alpine linux

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -11,8 +11,9 @@ which curl 1>/dev/null 2>&1 \
     || (echo "Please install curl" && exit 1)
 
 VIM_DEPS=./deps
+ARG=$1
 
-# prevent creating .npm or .yarn directories in home directory
+# prevent creating .npm directory in home directory
 OLD_HOME=$HOME
 HOME=/tmp
 
@@ -20,23 +21,43 @@ ls $VIM_DEPS 1>/dev/null 2>&1 || mkdir -p $VIM_DEPS
 
 download_nodejs()
 {
-    curl https://nodejs.org/dist/v12.4.0/node-v12.4.0-linux-x64.tar.xz -o $VIM_DEPS/nodejs.tar.xz -s
-    tar -xf $VIM_DEPS/nodejs.tar.xz -C $VIM_DEPS 1>/dev/null
-    rm -f $VIM_DEPS/*.tar.xz
-    mv $VIM_DEPS/node* $VIM_DEPS/nodejs
+    case $ARG in
+        alpine) download_nodejs_alpine ;;
+        *)      download_nodejs_other ;;
+    esac
+
+    return 0
+}
+
+download_nodejs_other()
+{
+    curl https://nodejs.org/dist/v12.4.0/node-v12.4.0-linux-x64.tar.xz -o ${VIM_DEPS}/nodejs.tar.xz -s
+    tar -xf ${VIM_DEPS}/nodejs.tar.xz -C $VIM_DEPS 1>/dev/null
+    rm -f ${VIM_DEPS}/*.tar.xz
+    mv ${VIM_DEPS}/node* ${VIM_DEPS}/nodejs
+
+    return 0
+}
+
+download_nodejs_alpine()
+{
+    curl -LSs https://github.com/oznu/alpine-node/releases/download/13.4.0/node-v13.4.0-linux-x86_64-alpine.tar.gz -o ${VIM_DEPS}/nodejs.tar.gz
+    tar -xf ${VIM_DEPS}/nodejs.tar.gz -C $VIM_DEPS 1>/dev/null
+    rm -f ${VIM_DEPS}/nodejs.tar.gz
+    mv ${VIM_DEPS}/local ${VIM_DEPS}/nodejs
 
     return 0
 }
 
 install_nodejs()
 {
-    sed -i --follow-symlinks "s|/usr/bin/env node|$(pwd)/${VIM_DEPS}/nodejs/bin/node|g" $VIM_DEPS/nodejs/bin/npm
-    sed -i --follow-symlinks "s|/usr/bin/env node|$(pwd)/${VIM_DEPS}/nodejs/bin/node|g" $VIM_DEPS/nodejs/bin/npx
+    sed -i --follow-symlinks "s|/usr/bin/env node|$(pwd)/${VIM_DEPS}/nodejs/bin/node|g" ${VIM_DEPS}/nodejs/bin/npm
+    sed -i --follow-symlinks "s|/usr/bin/env node|$(pwd)/${VIM_DEPS}/nodejs/bin/node|g" ${VIM_DEPS}/nodejs/bin/npx
 
     return 0
 }
 
-ls $VIM_DEPS/nodejs 1>/dev/null 2>&1 \
+ls ${VIM_DEPS}/nodejs 1>/dev/null 2>&1 \
     || (download_nodejs && install_nodejs)
 
 HOME=$OLD_HOME
